f9b0ec2be0ff3bad3506dafad3880ba56cd8c74a
Ivan_Rash <bazalijivan2002@yandex.ru>
2021-04-01 12:41:44 +0300

    Bug fix

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -50,13 +50,12 @@ int main() {
     ofstream fout("output.txt");
     map<string , pair<int, vector<double>>> stops_of_the_street, bus_stops_of_the_route, trolleybus_stops_of_the_route, tram_stops_of_the_route;
     vector<Stop> stoppages;
-    string street_with_maximum_stops, bus_route_with_maximum_stops, trolleybus_route_with_maximum_stops, tram_route_with_maximum_stops, maximum_bus_route_length, maximum_trolleybus_route_length, maximum_tram_route_length;
     xml_document main_file;
     xml_parse_result result = main_file.load_file("D:/Ivan/ITMO/Cpp_projects/homework3/data.xml");
     if (!result)
         return -1;
     else {
-        for (xml_node obj : main_file.child("dataset").children("transport_station")) {
+        for (xml_node obj : main_file.child("dataset").children("transport_station")){
             Stop current_stop;
             current_stop.set_transport(obj.child_value("type_of_vehicle"));
             current_stop.set_streets(obj.child_value("location"));
@@ -65,7 +64,7 @@ int main() {
             stoppages.push_back(current_stop);
         }
     }
-    for (Stop current : stoppages) {
+    for (Stop current : stoppages){
         for(string street : current.get_streets())
             if (!stops_of_the_street.contains(street) && street != "0")
                 stops_of_the_street[street].first = 1;
@@ -82,8 +81,8 @@ int main() {
     }
     fout << "Маршрут автобуса с наибольшим числом остановок: " << find_maximum_stops(bus_stops_of_the_route) << endl;
     fout << "Маршрут автобуса с наибольшей длиной: " << find_maximum_length(bus_stops_of_the_route) << endl;
-    fout << "Маршрут троллейбуса с наибольшим числом остановок: " << trolleybus_route_with_maximum_stops << endl;
-    fout << "Маршрут троллейбуса с наибольшей длиной: " << find_maximum_stops(trolleybus_stops_of_the_route) << endl;
+    fout << "Маршрут троллейбуса с наибольшим числом остановок: " << find_maximum_stops(trolleybus_stops_of_the_route) << endl;
+    fout << "Маршрут троллейбуса с наибольшей длиной: " << find_maximum_length(trolleybus_stops_of_the_route) << endl;
     fout << "Маршрут трамвая с наибольшим числом остановок: " << find_maximum_stops(tram_stops_of_the_route) << endl;
     fout << "Маршрут трамвая с наибольшей длиной: " << find_maximum_length(tram_stops_of_the_route) << endl;
     fout << "Улица с наибольшим числом остановок: " << find_maximum_stops(stops_of_the_street) << endl;
